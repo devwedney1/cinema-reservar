28d584338007b58b2691a63fe6c8f15e9c69649c
Lucas Wedney <devwedney@outlook.com>
2025-05-16 11:09:04 -0300

    FEAT: populado o banco de dados com os dados as table que tem enum

--- app/database/cinema.sql
+++ app/database/cinema.sql
@@ -1,3 +1,6 @@
+-- Apagar todas as configuração do banco que tiver, para roda novamente do inicio
+drop database if exists cinema;
+
 -- Criação do banco de dados
 CREATE DATABASE IF NOT EXISTS cinema;
 USE cinema;
@@ -10,7 +13,10 @@ CREATE TABLE IF NOT EXISTS categoria_filmes
     created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     deleted_at     TIMESTAMP NULL
-    );
+);
+
+-- Populado o banco de dados
+INSERT INTO categoria_filmes(nome_categoria) VALUES ('Ação'), ('Aventura'), ('Comédia'), ('Drama'), ('Ficção Científica'), ('Terror'), ('Romance'), ('Animação');
 
 -- Tabela de filmes
 CREATE TABLE IF NOT EXISTS filmes
@@ -30,11 +36,14 @@ CREATE TABLE IF NOT EXISTS filmes
 CREATE TABLE IF NOT EXISTS salas
 (
     id         INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    nome       ENUM ('A', 'B', 'C'),
+    nome_sala       ENUM ('A', 'B', 'C'),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP NULL
-    );
+);
+
+-- Populado o banco de dados
+INSERT INTO salas (nome_sala) VALUES ('A'), ('B'), ('C');
 
 -- Tabela de sessões de filmes
 CREATE TABLE IF NOT EXISTS sessoes_filmes
@@ -59,7 +68,7 @@ CREATE TABLE IF NOT EXISTS horario_sessoes
     updated_at        TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     deleted_at        TIMESTAMP NULL,
     FOREIGN KEY (sessoes_filmes_id) REFERENCES sessoes_filmes (id)
-    );
+);
 
 -- Tabela de cadeiras
 CREATE TABLE IF NOT EXISTS cadeiras
@@ -71,7 +80,7 @@ CREATE TABLE IF NOT EXISTS cadeiras
     updated_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     deleted_at     TIMESTAMP NULL,
     FOREIGN KEY (sala_id) REFERENCES salas (id)
-    );
+);
 
 -- Tabela de formas de pagamento
 CREATE TABLE IF NOT EXISTS forma_pagamento
@@ -81,7 +90,10 @@ CREATE TABLE IF NOT EXISTS forma_pagamento
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP NULL
-    );
+);
+
+-- Populado o banco de dados
+INSERT INTO forma_pagamento (pagamento) VALUES ('dinheiro'), ('cartao debito'), ('cartao credito'), ('bitcoin');
 
 -- Tabela de ingressos (vendas)
 CREATE TABLE IF NOT EXISTS ingressos
@@ -97,3 +109,6 @@ CREATE TABLE IF NOT EXISTS ingressos
     FOREIGN KEY (cadeira_id) REFERENCES cadeiras (id),
     FOREIGN KEY (forma_pagamento_id) REFERENCES forma_pagamento (id)
 );
+
+-- Populado o banco de dados
+INSERT INTO ingressos (status) VALUES  ('pago'), ('cancelado');
